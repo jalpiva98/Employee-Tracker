f2afdbf5319dba2e55345f13748f9470497d4a0f
Alejandro Piñón <jesuspiva1998@hotmail.com>
2023-09-05 19:13:27 -0600

    Created database structure and initial seeds

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+-- Inserts names of departments into department table
+INSERT INTO department
+  (name)
+VALUES
+  ('Design'),
+  ('Lightning'),
+  ('Structure'),
+  ('Sales');
+
+-- Inserts roles of employee into role table
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Jr. Architect', 85000, 1),
+  ('Engineer', 75000, 2),
+  ('Lightningh designer', 125000, 3),
+  ('Project salesman', 200000, 4);
+
+-- Inserts employee information into employee table
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Azul', 'Lobos', 1, 4),
+  ('Cristina', 'Rios', 2, 3),
+  ('Jesus', 'Piñon', 3, 1),
+  ('Estefany', 'Garcia', 4, 5);
